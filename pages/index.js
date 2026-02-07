86c1c63520892c7d1ae4dce573830fa89fc5232b
Felipe <felipeccotta@gmail.com>
2026-02-07 13:33:48 +0100

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 function Home() {
-  return <h1>Teste</h1>
+  return <h1>Teste</h1>;
+}
+
+function test() {
+  console.log("teste");
+}
+
+function test2() {
+  console.log("fomatação errada");
 }
 
 export default Home;
-
